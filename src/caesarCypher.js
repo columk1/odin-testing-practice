feb48a0752d5e0fbb5c9f343b7ff2efd84eb22c4
Colum Kelly <columk1@users.noreply.github.com>
2023-09-10 16:08:56 -0700

    Add caesarCypher function and tests

---
+++ src/caesarCypher.js
@@ -0,0 +1,11 @@
+function shift(char, k, caseCode) {
+  return String.fromCharCode(((char.charCodeAt() - caseCode + k) % 26) + caseCode)
+}
+
+function caesarCypher(input, k = 10) {
+  return input
+    .replace(/[a-z]/g, (char) => shift(char, k, 97))
+    .replace(/[A-Z]/g, (char) => shift(char, k, 65))
+}
+
+module.exports = caesarCypher
---
+++ src/caesarCypher.test.js
@@ -0,0 +1,25 @@
+const caesarCypher = require('./caesarCypher')
+
+test('Lowercase without wrapping from z to a', () => {
+  expect(caesarCypher('hello', 10)).toBe('rovvy')
+})
+
+test('Uppercase without wrapping from Z to A', () => {
+  expect(caesarCypher('HELLO', 10)).toBe('ROVVY')
+})
+
+test('Lowercase with wrapping from z to a', () => {
+  expect(caesarCypher('z', 1)).toBe('a')
+})
+
+test('Uppercase with wrapping from Z to A', () => {
+  expect(caesarCypher('Z', 1)).toBe('A')
+})
+
+test('Mixed case: (Zz, 1) => Aa', () => {
+  expect(caesarCypher('Zz', 1)).toBe('Aa')
+})
+
+test('Includes non alphabetical characters: (g#Y7, 1) => h#Z7', () => {
+  expect(caesarCypher('g# Y7.', 1)).toBe('h# Z7.')
+})
